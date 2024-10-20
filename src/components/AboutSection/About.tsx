33eee90d1ca99635bd68ac44f4e6574a7e130ff4
Eli Whitaker <eli.p.p.whitaker@gmail.com>
2024-10-19 21:51:16 -0700

    Changed the header, added logout, change account, centered images, and added a Get Started btton

--- src/components/AboutSection/About.tsx
+++ src/components/AboutSection/About.tsx
@@ -1,7 +1,20 @@
 import Information from "./InformationAboutProduct";
 import ListenToMusic from "./ListenToMusic";
 import AIUsage from "./AIUsage";
-export default function About() {
+import Link from "next/link";
+import { LoginWithGoogle } from "@/app/login/actions";
+
+import { createClient } from "@/utils/supabase/server";
+
+export default async function About() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    console.error(error);
+  }
+
   return (
     <div className="flex flex-col">
       <div className="bg-seasalt-500">
@@ -9,10 +22,30 @@ export default function About() {
           How does it work?
         </h1>
       </div>
-      <div className="w-full h-full justify-center relative flex flex-row p-2 gap-5 bg-seasalt-500">
-        <Information />
-        <ListenToMusic />
-        <AIUsage />
+      <div className="flex flex-col  bg-seasalt-500 ">
+        <div className="w-full h-full justify-center relative flex flex-row p-2 gap-5 bg-seasalt-500">
+          <Information />
+          <ListenToMusic />
+          <AIUsage />
+        </div>
+        <div className="mb-10 bg-seasalt-500 flex justify-center items-center">
+          {data.user ? (
+            <Link href="/dashboard">
+              <p className="text-6xl flex focus-center text-gray-600 gruppo">
+                Get Started
+              </p>
+            </Link>
+          ) : (
+            <form action={LoginWithGoogle}>
+              <button
+                className="text-6xl flex focus-center text-gray-600 gruppo"
+                type="submit"
+              >
+                Get Started
+              </button>
+            </form>
+          )}
+        </div>
       </div>
     </div>
   );
