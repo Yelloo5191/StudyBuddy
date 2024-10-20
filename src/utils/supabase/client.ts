2d760c53dea82c6727efbb36eabb100719c866f4
Hovhannes Muradyan <76452706+Yelloo5191@users.noreply.github.com>
2024-10-20 00:03:09 -0700

    env

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 export function createClient() {
   return createSupabaseClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_URL!,
     process.env.SUPABASE_ANON_KEY!
   );
 }
