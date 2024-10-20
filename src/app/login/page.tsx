8a4fcbdc88f9f545682959710c86a1783a06679d
Hovhannes Muradyan <76452706+Yelloo5191@users.noreply.github.com>
2024-10-19 17:05:33 -0700

    WORKING OAUTH WOO HOO

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,6 @@
-"use client";
-
-import { login, signup } from "./actions";
-import { useRouter } from 'next/navigation';
-import { createClient } from "@/utils/supabase/client";
+import SignInButton from "./SignInButton";
 
 export default function LoginPage() {
-  const supabase = createClient()
-  const router = useRouter();
-
-  const handleLogin = async () => {
-    await supabase.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
-          redirectTo: `https://ahjeuzhakgxsolkvdhpy.supabase.co/auth/v1/callback`,
-        },
-    });
-  };
-
   return (
     <div className="flex flex-col bg-dark_light w-full h-screen gap-4 p-4">
       <div className="w-full h-16 flex flex-row justify-center items-center bg-light_gray rounded-lg">
@@ -24,12 +8,7 @@ export default function LoginPage() {
       </div>
       <div className="flex gap-4 h-full">
         <div className="w-1/2 h-1/2 flex flex-col gap-8 p-8 bg-light_gray rounded-lg">
-          <button 
-            className="bg-blue-500 text-white p-2 rounded" 
-            onClick={handleLogin}
-          >
-            Sign in with Google
-          </button>
+          <SignInButton />
         </div>
       </div>
     </div>
